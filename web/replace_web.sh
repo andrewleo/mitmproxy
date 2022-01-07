4ea692e09462a5d7cceb9bfc525d01b22b83e61c
andrewliu <andrewliu@didiglobal.com>
2022-01-07 21:02:48 +0800

    Added: replace script

---
+++ web/replace_web.sh
@@ -0,0 +1,3 @@
+docker cp ../mitmproxy/tools/web/static/app.js mitm8083:/usr/local/lib/python3.9/site-packages/mitmproxy/tools/web/static/app.js
+docker cp ../mitmproxy/tools/web/static/app.css mitm8083:/usr/local/lib/python3.9/site-packages/mitmproxy/tools/web/static/app.css
+docker cp ../mitmproxy/tools/web/static/vendor.css mitm8083:/usr/local/lib/python3.9/site-packages/mitmproxy/tools/web/static/vendor.css
