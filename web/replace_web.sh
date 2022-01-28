da1468782dabfecc0b081aea317da4adabb7b27d
andrewliu <andrewliu@didiglobal.com>
2022-01-28 16:32:13 +0800

    Added: path fixed

--- web/replace_web.sh
+++ web/replace_web.sh
@@ -2,4 +2,4 @@ docker cp ../mitmproxy/tools/web/static/app.js mitm8083:/usr/local/lib/python3.9
 docker cp ../mitmproxy/tools/web/static/app.css mitm8083:/usr/local/lib/python3.9/site-packages/mitmproxy/tools/web/static/app.css
 docker cp ../mitmproxy/tools/web/static/vendor.css mitm8083:/usr/local/lib/python3.9/site-packages/mitmproxy/tools/web/static/vendor.css
 docker cp ../mitmproxy/tools/web/app.py mitm8083:/usr/local/lib/python3.9/site-packages/mitmproxy/tools/web/app.py
-docker cp ../mitmproxy/add_header.py mitm8083:/home/mitmproxy/add_header.py
+docker cp ../add_header.py mitm8083:/home/mitmproxy/add_header.py
