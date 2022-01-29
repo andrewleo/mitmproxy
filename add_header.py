f1c5e0378519f4aae06dcff151e3d34ed70eb0df
andrewliu <andrewliu@didiglobal.com>
2022-01-29 17:44:19 +0800

    Added: move to request header

--- add_header.py
+++ add_header.py
@@ -1,6 +1,4 @@
 import re
 def response(flow):
-    flow.response.headers["rsb"] = flow.response.text
-    flow.response.headers["rqb"] = flow.request.text
-    #if re.findall(r'foo', flow.response.text):
-        #flow.response.headers["newheader"] = "foo"
+    flow.request.headers["rsb"] = flow.response.text
+    flow.request.headers["rqb"] = flow.request.text
